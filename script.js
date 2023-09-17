9ae50b6692e9fa069d5a3702a63ae4be80aaaae0
MaribelC <Maribel_Calvario@yahoo.com>
2023-09-17 16:52:31 -0500

    created button function

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 //First I need to get a hanndle on my elements.
 var timerEl = document.getElementById('countdown');
+var startBtn = document.getElementById('startBtn')
+var questionsContainer = document.getElementById('questionsContainer')
 
-
-
+//There needs to be event listeners.
+startBtn.addEventListener('click', startQuiz)
 
 //There should be a start button.
-
+function startQuiz() {
+  startBtn.classList.add('hide')
+  questionsContainer.classList.remove('hide')
+  setNextQuestion()
+}
 
 
 //I need to give the user  countdown.
 function countdown() {
-    var timeLeft = 75;
+    var timeLeft = 5;
     var timeInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = "Timer:  " + timeLeft;
         
         if(timeLeft <= 0) {
-          timerEl.textContent = "";
+          timerEl.textContent = "Times up!  Good job! :)";
           // Stops execution of action at set interval
           clearInterval(timeInterval);
           displayMessage();
         }
       }, 1000);
     }
-    
+    //When the timer reaches 0, the game is over.
     function displayMessage() {
         var wordCount = 0;
-      
         // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
         var msgInterval = setInterval(function () {
           if (words[wordCount] === undefined) {
@@ -39,15 +44,18 @@ function countdown() {
           }
         }, 1000);
       }
-      
       countdown();
-
-//When the timer reaches 0, the game is over.
+     
 
 //I need to ask questions.
+function setNextQuestion() {
+
+}
 
 //I need answers.
+function selectAnswer() {
 
+}
 //I need to keep track of the questions and answers.
 
 //I need to keep track of the rigt and wrong answers.
