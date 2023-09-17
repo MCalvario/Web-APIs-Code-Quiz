d18f626f67ffb274fd54cd2855b9cd8d88bed2db
MaribelC <Maribel_Calvario@yahoo.com>
2023-09-17 02:20:32 -0500

    pseudo coding for js and html started

---
+++ script.js
@@ -0,0 +1,26 @@
+//First I need to get a hanndle on my elements.
+
+//There should be a start button.
+
+//I need to give the user  countdown.
+
+//When the timer reaches 0, the game is over.
+
+//I need to ask questions.
+
+//I need answers.
+
+//I need to keep track of the questions and answers.
+
+//I need to keep track of the rigt and wrong answers.
+
+//I will need an array of objects to keep track.
+
+//I need to display the time remaining.
+
+//I need to display a button where we can view high scores.
+
+//I need to create a box where user can enter initials at the end of the game.
+
+//User should be able to save initials and score.
+
