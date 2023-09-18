7eccd365df4dd9fbd7e6840e874bee6fca0967d5
MaribelC <Maribel_Calvario@yahoo.com>
2023-09-18 15:13:17 -0500

    Added function for questions answered

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 var timerEl = document.getElementById('countdown');
 var button = document.querySelector('startBtn');
 var nextButton = document.createElement('button');
-var question = document.getElementById("question")
-var options = document.getElementById("options")
+var question = document.getElementById("question");
+var options = document.getElementById("options");
+var button = document.querySelector('viewHighScore');
+
+
 
 //There should be a start button.
 //There needs to be an event listener for the start quiz button.
@@ -21,6 +24,10 @@ nextButton.textContent = 'Next!';
 document.body.appendChild(nextButton);
 
 //I need to display a button where we can view high scores.
+viewHighScore.addEventListener('click', showResults);
+function showResults () {
+  alert('show results')
+}
 
 //I need to create a box where user can enter initials at the end of the game.
 
@@ -61,11 +68,66 @@ var Questions = [{
   { text: "h1", isCorrect: true },
   { text: "h6", isCorrect: false }
   ]
-
 }
-
 ]
 
+var currentQuestion = 0
+var score = 0
+
+function loadQuestion() {
+ 
+
+  question.textContent = Questions[currentQuestion].question;
+  options.innerHTML = ""
+
+  for (let i = 0; i < Questions[currentQuestion].answer.length; i++) {
+      var choicesdiv = document.createElement("div");
+      var choice = document.createElement("input");
+      var choiceLabel = document.createElement("label");
+
+      choice.type = "checkbox";
+      choice.name = "answer";
+      choice.value = i;
+
+      choiceLabel.textContent = Questions[currentQuestion].answer[i].text;
+
+      choicesdiv.appendChild(choice);
+      choicesdiv.appendChild(choiceLabel);
+      options.appendChild(choicesdiv);
+  }
+}
+
+loadQuestion();
+
+function loadScore() {
+  var totalScore = document.getElementById("score")
+  totalScore.textContent = `You scored ${score} out of ${Questions.length}`
+}
+
+
+function nextQuestion() {
+  if (currentQuestion < Questions.length - 1) {
+      currentQuestion++;
+      loadQuestion();
+  } else {
+      document.getElementById("options").remove()
+      document.getElementById("question").remove()
+      document.getElementById("btn").remove()
+      loadScore();
+  }
+}
+
+function checkAns() {
+  var selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
+
+  if (Questions[currentQuestion].answer[selectedAns].isCorrect) {
+      score++;
+      console.log("Correct")
+      nextQuestion();
+  } else {
+      nextQuestion();
+  }
+}
 
 
 
